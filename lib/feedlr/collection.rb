50db6cbe3eae256fecef6abd65423bec840fc03a
Khaled alHabache <khellls@gmail.com>
2014-06-06 20:24:56 +0300

    move to delegation instead of subclassing for Feedlr::Collection

--- lib/feedlr/collection.rb
+++ lib/feedlr/collection.rb
@@ -1,11 +1,12 @@
+require 'delegate'
 module Feedlr
   # When the response is of Array type,
   #  it creates an array of Feedlr::Base or plain types elements
-  class Collection < Array
+  class Collection < SimpleDelegator
     # Initializer
     # @param [Array] data
     # @return [Feedlr::Collection]
-    def initialize(data)
+    def initialize(data = [])
       super([])
       data.each do |e|
         self.<<(e.is_a?(Hash) ? Feedlr::Base.new(e) : e)
--- spec/feedlr/collection_spec.rb
+++ spec/feedlr/collection_spec.rb
@@ -3,6 +3,11 @@ require 'helper'
 describe Feedlr::Collection do
 
   describe '::new' do
+    it 'should have empty data when initialized with no data' do
+      collection = Feedlr::Collection.new
+      expect(collection.first).to be_nil
+    end
+
     it 'should have Feedlr::Base elements when the elements are hashes' do
       collection = Feedlr::Collection.new([{ a: 1 }, { b: 2 }])
       expect(collection.first).to be_a(Feedlr::Base)
@@ -10,12 +15,6 @@ describe Feedlr::Collection do
     it 'should have normal elements when the elements are not hashes' do
       collection = Feedlr::Collection.new([1, { z: 5 }])
       expect(collection.first).to be_a(Integer)
-    end
-  end
-
-  describe '#is_a?' do
-    it 'should be subclass of Array' do
-      expect(Feedlr::Collection.new([{ a: 1 }])).to be_an(Array)
     end
   end
 end
