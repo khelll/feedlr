3543cfa96ee3b0a70b0ba0b627d8e90c0a4f64a5
Khaled alHabache <khellls@gmail.com>
2014-07-13 02:02:16 +0300

    remove duplicate method call

--- lib/feedlr/rate_limit.rb
+++ lib/feedlr/rate_limit.rb
@@ -10,12 +10,15 @@ module Feedlr
     # @option attrs [String] :x-ratelimit-remaining
     # @return [Feedlr::RateLimit]
     def initialize(attrs =  {})
-      @count =
-        attrs['x-ratelimit-count'].to_i if attrs['x-ratelimit-count']
-      @limit =
-        attrs['x-ratelimit-limit'].to_i if attrs['x-ratelimit-limit']
-      @remaining =
-        attrs['x-ratelimit-remaining'].to_i if attrs['x-ratelimit-remaining']
+      @count = get_value(attrs['x-ratelimit-count'])
+      @limit = get_value(attrs['x-ratelimit-limit'])
+      @remaining = get_value(attrs['x-ratelimit-remaining'])
+    end
+
+    private
+
+    def get_value(value)
+      value.to_i if value
     end
   end
 end
